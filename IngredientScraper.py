d52d827d3b9266c51a68d1df63f60f778bb1efcb
Andrew Mullen <Andrewjmullen45@gmail.com>
2022-12-09 14:20:37 -0500

    working script

--- IngredientScraper.py
+++ IngredientScraper.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup
 import requests
 
 
-url = 'https://www.gimmesomeoven.com/easy-beef-stroganoff-recipe/' #input('Enter a URL: ')
+url = 'https://www.delish.com/cooking/recipe-ideas/a19636089/creamy-tuscan-chicken-recipe/' #input('Enter a URL: ')
 
 # Make a GET request to fetch the raw HTML content
 html_content = requests.get(url).text
@@ -12,8 +12,7 @@ html_content = requests.get(url).text
 # Parse the html content
 soup = BeautifulSoup(html_content, "lxml")
 
-# Find all the ingredients in the recipe
-ingredients = soup.find_all('div', attrs = {'class':'tasty-recipes-ingredients-body'})
+
 #working to get all classes that contain list
 class_ingredients = []
 classes = []
@@ -23,19 +22,27 @@ for element in soup.find_all('div'):
 for cls in classes:
     if 'ingredients' in cls:
         class_ingredients.append(cls)
-print(class_ingredients)
+#print(class_ingredients)
 
 #find if div contains <li> for the classes in class_ingredients
 div_list=[]
 for list in class_ingredients:
-    print(list)
-    if list.get('li') is not None:
-        div_list.append(list.get('li'))
-print(div_list)
+    #print("list:", list)
+
+    #find div element classes contained in class_ingredients
+    div = soup.find('div', class_=list)
+    #print('div:', div)
+
+    #check if div contains list
+    if div.find("ul") or div.find("ol"):
+        div_list.append(div)
+
+#print("div that contains list:", div_list)
     
+# Find all the ingredients in the recipe
+ingredients = soup.find_all('div', attrs = {'class':list})
 
 #print ingredients
 for ingredient in ingredients:
 
     print(ingredient.text)
-
