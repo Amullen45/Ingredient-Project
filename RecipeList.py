7dd874c0081593c56c977eb239f2fb4fe2a22876
Andrew Mullen <Andrewjmullen45@gmail.com>
2022-12-14 20:41:08 -0500

    added code to clean up ingredients class

--- RecipeList.py
+++ RecipeList.py
@@ -9,6 +9,9 @@ class Recipe_list:
         self.items=[]
 
     #define a method to add an object to the list
+    def __init__(self):
+        self.items = []
+
     def add(self, item):
         self.items.append(item)
 
@@ -22,18 +25,18 @@ class Recipe_list:
     
 #get recipe name
 recipe = IngredientScraper.title
-print(recipe)
+#print(recipe)
 
 #get ingredients list
 ingredients = IngredientScraper.ingredient_list
-print(ingredients)
+#print(ingredients)
 
 #function to make recipe title a python variable
 def python_variable(str):
     return str.lower().replace(' ', '_')
 
 variable = python_variable(recipe)
-print(variable)
+#print(variable)
 
 recipes = Recipe_list()
 
@@ -41,3 +44,20 @@ recipes.add({'recipe': recipe, 'Ingredients': ingredients})
 
 for i in range(len(recipes.items)):
     print(recipes.get(i))
+
+"""
+Working formula to clean up the ingredients list, 
+just need to figure where it fits into the script,
+so that it prints automatically
+"""
+# Strip whitespace from each element in the array
+cleaned_array = [x.strip() for x in ingredients]
+
+# Remove empty elements from the array
+cleaned_array = list(filter(None, cleaned_array))
+
+#print(cleaned_array)
+
+# Separate the string into a list where each item is separated by "\n"
+string_list = cleaned_array[0].replace('\xa0', ' ').split("\n")
+#print(string_list)
